66c56c243d5fcc3c313c359f4f49fb45a4d5ffa8
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2019-04-26 21:29:50 +0200

    Make recognition even faster and use less GPU memory

--- clf_object_recognition_tensorflow/src/clf_object_recognition_tensorflow/recognize.py
+++ clf_object_recognition_tensorflow/src/clf_object_recognition_tensorflow/recognize.py
@@ -7,6 +7,10 @@ class TensorflowRecognition:
 
     def __init__(self):
         self.labels = []
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth=True #If true, the allocator does not pre-allocate the entire specified GPU memory region, instead starting small and growing as needed.
+        config.gpu_options.per_process_gpu_memory_fraction=0.25 #Don't use so much GPU memory!
+        self.sess=tf.Session(config=config) #If the recognition is used very rarely, it might make sense to only open a session when it is needed and close it afterwards (with a context manager, "with tf.Session ...")
 
 
     def load_graph(self, graph_path, label_path):
@@ -20,22 +24,19 @@ class TensorflowRecognition:
 
 
     def recognize(self,filename):
-        config = tf.ConfigProto()
-        config.gpu_options.allow_growth=True #If true, the allocator does not pre-allocate the entire specified GPU memory region, instead starting small and growing as needed.
-        with tf.Session(config=config) as sess: #TODO do this in __init__? That way it only has to be done once and not for every image again
-            """1. Get result tensor"""
-            result_tensor = sess.graph.get_tensor_by_name("final_result:0")
+        """1. Get result tensor"""
+        result_tensor = self.sess.graph.get_tensor_by_name("final_result:0")
 
-            """2. Open Image and perform prediction"""
-            predictions = []
+        """2. Open Image and perform prediction"""
+        predictions = []
 
-            #TODO: read image from memory
-            with open(filename, 'rb') as f: #TODO instead of reading the image from a file, pass it as a numpy-array parameter to recognize(), see here https://stackoverflow.com/questions/40273109/convert-python-opencv-mat-image-to-tensorflow-image-data and here https://stackoverflow.com/questions/34484148/feeding-image-data-in-tensorflow-for-transfer-learning Problem: rgb or bgr?
-                predictions = sess.run(result_tensor, {'DecodeJpeg/contents:0': f.read()})
-                predictions = np.squeeze(predictions)
+        #TODO: read image from memory
+        with open(filename, 'rb') as f: #TODO instead of reading the image from a file, pass it as a numpy-array parameter to recognize(), see here https://stackoverflow.com/questions/40273109/convert-python-opencv-mat-image-to-tensorflow-image-data and here https://stackoverflow.com/questions/34484148/feeding-image-data-in-tensorflow-for-transfer-learning Problem: rgb or bgr?
+            predictions = self.sess.run(result_tensor, {'DecodeJpeg/contents:0': f.read()})
+            predictions = np.squeeze(predictions)
 
-            """3. Construct list with labels and probabilities"""
-            result = zip(list(range(len(predictions))), predictions)
+        """3. Construct list with labels and probabilities"""
+        result = zip(list(range(len(predictions))), predictions)
 
         # return sorted list
         sorted_result = sorted(result, key=lambda x: x[1])
