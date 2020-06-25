58faaf576988413145733768fc32f381a5eb9b9d
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2020-06-25 14:42:16 +0200

    Correct and improve includes 5

--- clf_object_recognition_rviz/include/clf_object_recognition_rviz/detection_3d_array_display.h
+++ clf_object_recognition_rviz/include/clf_object_recognition_rviz/detection_3d_array_display.h
@@ -4,9 +4,9 @@
 #include <vision_msgs/Detection3DArray.h>
 
 #include "clf_object_recognition_rviz/detection_3d_visual.h"
-#include "clf_object_recognition_rviz/bounding_box_visual.h"
 #include "rviz/message_filter_display.h"
 #endif
+#include "clf_object_recognition_rviz/bounding_box_visual.h"
 
 #include <mutex>
 
