f7768ec5e0a736d6036a52781a1bab2b67f19557
Timo Beckmann <tbeckmann@techfak.de>
2026-08-18 14:47:38 +0200

    small addition to last commit

--- clf_object_recognition_3d/src/detector.cpp
+++ clf_object_recognition_3d/src/detector.cpp
@@ -515,7 +515,7 @@ bool Detector::ServiceDetect3D(clf_object_recognition_msgs::Detect3D::Request& r
         d3d.results.push_back(hyp);
       }
       res.detections.push_back(d3d);
-      if(use_masks) {
+      if(use_masks && config.use_masks) {
         res.masks.push_back(mask);
       }
     }
