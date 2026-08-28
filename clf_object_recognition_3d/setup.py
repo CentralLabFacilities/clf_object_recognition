83d5dce3f46d30d24e62163763483a2b9c6910cd
Leroy Rügemer <lruegeme@techfak.uni-bielefeld.de>
2026-08-28 16:50:28 +0200

    wip cleanup

--- clf_object_recognition_3d/setup.py
+++ clf_object_recognition_3d/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['clf_object_recognition_3d'],
+    packages=['pyaudio_capture'],
     package_dir={'': 'src'}
 )
 
